980f908dced130dbfd63d6f0420b883e374919c4
Matthew David Elgert <mdelgert@yahoo.com>
2024-08-16 16:21:51 -0400

    add subscriber

---
+++ ClipboardUtil.MqttSubscriber/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+
+namespace ClipboardUtil.MqttSubscriber
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Load configuration from appsettings.json
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var mqttSettings = configuration.GetSection("MqttSettings").Get<MqttSettings>();
+
+            string broker = mqttSettings.Broker;
+            int port = mqttSettings.Port;
+            string clientId = mqttSettings.ClientId ?? Guid.NewGuid().ToString();
+            string topic = mqttSettings.Topic;
+            string username = mqttSettings.Username;
+            string password = mqttSettings.Password;
+            string certificatePath = mqttSettings.CertificatePath;
+
+            // Create a MQTT client factory
+            var factory = new MqttFactory();
+
+            // Create a MQTT client instance
+            var mqttClient = factory.CreateMqttClient();
+
+            //TODO would need to convert PathTotext
+            X509Certificate2Collection caChain = new X509Certificate2Collection();
+            string cert = File.ReadAllText(certificatePath);
+            caChain.ImportFromPem(cert);
+
+            // Create MQTT client options
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(broker, port) // MQTT broker address and port
+                .WithCredentials(username, password) // Set username and password
+                .WithClientId(clientId)
+                .WithCleanSession()
+                .WithTlsOptions(
+                o =>
+                {
+                    // The used public broker sometimes has invalid certificates.
+                    // This sample accepts all certificates. This should not be used in live environments.
+                    // Works with EMQX platform
+                    o.WithCertificateValidationHandler(_ => true);
+
+                    // The default value is determined by the OS. Set manually to force version.
+                    o.WithSslProtocols(SslProtocols.Tls12);
+
+                    // Please provide the file path of your certificate file.
+                    o.WithTrustChain(caChain);
+                })
+
+                .Build();
+
+            // Connect to MQTT broker
+            var connectResult = await mqttClient.ConnectAsync(options);
+
+            if (connectResult.ResultCode == MqttClientConnectResultCode.Success)
+            {
+                Console.WriteLine("Connected to MQTT broker successfully.");
+
+                // Subscribe to a topic
+                await mqttClient.SubscribeAsync(topic);
+
+                // Callback function when a message is received
+                mqttClient.ApplicationMessageReceivedAsync += e =>
+                {
+                    Console.WriteLine($"Received message: {Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment)}");
+                    return Task.CompletedTask;
+                };
+
+                //Stay connected
+                Console.ReadKey();
+
+                // Unsubscribe and disconnect
+                await mqttClient.UnsubscribeAsync(topic);
+                await mqttClient.DisconnectAsync();
+            }
+            else
+            {
+                Console.WriteLine($"Failed to connect to MQTT broker: {connectResult.ResultCode}");
+            }
+        }
+    }
+
+    public class MqttSettings
+    {
+        public string Broker { get; set; }
+        public int Port { get; set; }
+        public string ClientId { get; set; }
+        public string Topic { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string CertificatePath { get; set; }
+    }
+}
---
+++ ClipboardUtil.MqttSubscriber/README.md
@@ -0,0 +1,8 @@
+https://www.nuget.org/packages/MQTTnet/
+Install-Package MQTTnet -Version 4.3.6.1152
+Install-Package Microsoft.Extensions.Hosting
+Install-Package Microsoft.Extensions.Configuration.Json
+
+dotnet add package MQTTnet -Version 4.3.6.1152
+dotnet add package Microsoft.Extensions.Hosting
+dotnet add package Microsoft.Extensions.Configuration.Json
